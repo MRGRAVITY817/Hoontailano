bb11884dc345949361005cf1ddc43d4cf7581c08
Hoon Wee <mrgravity817@gmail.com>
2023-08-16 16:28:44 +0900

    refactor: groupMenuByCategory will return empty list without conditional logic

--- Hoontaliano/Utils/groupMenuByCategory.swift
+++ Hoontaliano/Utils/groupMenuByCategory.swift
@@ -8,12 +8,9 @@
 import Foundation
 
 func groupMenuByCategory(_ menu: [MenuItem]) -> [MenuSection] {
-    guard menu.isEmpty == false else { return [] }
-    
     return Dictionary(grouping: menu, by: { $0.category })
         .map { key, value in
             MenuSection(items: value, category: key)
         }
         .sorted { $0.category > $1.category }
-        
 }
