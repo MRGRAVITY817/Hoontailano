485c1498e2f2613b2f829fc8988722df5913b8fd
Hoon Wee <mrgravity817@gmail.com>
2023-08-16 14:57:28 +0900

    Initial Commit

---
+++ Hoontaliano/HoontalianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoontalianoApp.swift
+//  Hoontaliano
+//
+//  Created by Hoon Wee on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct HoontalianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoontalianoUITests/HoontalianoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HoontalianoUITestsLaunchTests.swift
+//  HoontalianoUITests
+//
+//  Created by Hoon Wee on 2023/08/16.
+//
+
+import XCTest
+
+final class HoontalianoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
