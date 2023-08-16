f2a1f997d5e857ebb8d53476ba9be3b88ad633d4
Hoon Wee <mrgravity817@gmail.com>
2023-08-17 00:44:46 +0900

    test: non-spicy food should have only its name in section

---
+++ Hoontaliano/Views/MenuRow.ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MenuRow.ViewModel.swift
+//  Hoontaliano
+//
+//  Created by Hoon Wee on 2023/08/17.
+//
+
+import Foundation
+
+extension MenuRow {
+    struct ViewModel {
+        let text: String
+        
+        init(item: MenuItem) {
+            text = item.name
+        }
+    }
+}
---
+++ HoontalianoTests/MenuRowViewModelTests.swift
@@ -0,0 +1,22 @@
+//
+//  MenuRowViewModelTests.swift
+//  HoontalianoTests
+//
+//  Created by Hoon Wee on 2023/08/17.
+//
+
+@testable import Hoontaliano
+import XCTest
+
+final class MenuRowViewModelTests: XCTestCase {
+    
+    func test_WhenItemIsNotSpicy_TextIsItemNameOnly() {
+        let item = MenuItem.fixture(name: "name", spicy: false)
+        
+        let viewModel = MenuRow.ViewModel(item: item)
+        
+        XCTAssertEqual(viewModel.text, "name")
+    }
+    
+    func test_WhenItemIsSpicy_TextIsItemNameWithChiliEmoji() {}
+}
